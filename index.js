fe0197c120275c81198830d413dfaaa415a37fff
mmtakeuchi <mikemtaki@gmail.com>
2020-10-26 17:43:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let index = 0;
 
+    for (let i = 1; i < array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
+            index = i;
+        } 
+    }
+
+    array.splice(index, 1)
+    return min;
 }
 
 function selectionSort(array){
+    let newMin;
+    let sorted = [];
 
+    while (array.length > 0) {
+        newMin = findMinAndRemove(array);
+        sorted.push(newMin)
+    }
+
+    return sorted;
 }
