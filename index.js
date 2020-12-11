16bc6939b85a61eed16bdb80d2944104852f3552
Anil <anil_gurung15@yahoo.com>
2020-12-11 14:50:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+    let l = array.length
+    let min = array[0]
+    let minIndex
 
+    for (let i = 0; i < l; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            minIndex = i
+        }
+    }
+
+    //array.splice(index, howmany, item1, ....., itemX)
+    array.splice(minIndex, 1)
+    return min
 }
 
-function selectionSort(array){
+function selectionSort(array) {
+    let sortedArr = []
+    let newMin;
+    while (array.length != 0) {
+        newMin = findMinAndRemove(array)
+        sortedArr.push(newMin)
+    }
+    return sortedArr
 
 }
